63e84f2ed773debe75ac9200eb0981a085cb5c50
hussain3134 <95437954+hussain3134@users.noreply.github.com>
2026-02-19 12:34:31 +0530

    Remove duplicate ecommerce integrations entries

--- .github/scripts/compute_latest_versions.py
+++ .github/scripts/compute_latest_versions.py
@@ -69,10 +69,10 @@ def main():
         "erpnext": git_latest_tag("https://github.com/frappe/erpnext", "16"),
         "hrms": git_latest_tag("https://github.com/frappe/hrms", "16"),
         "crm": github_latest_release("frappe", "crm"),
+        "ecommerce_integrations_ref": "refs/heads/version-16",
+        "ecommerce_integrations_sha": git_branch_sha("https://github.com/frappe/ecommerce_integrations", "version-16"),
         "india_compliance_ref": "refs/heads/version-16",
         "india_compliance_sha": git_branch_sha("https://github.com/resilient-tech/india-compliance", "version-16")
-        "ecommerce_integrations_ref": "refs/heads/version-16",
-        "ecommerce_integrations_sha": git_branch_sha("https://github.com/frappe/ecommerce_integrations", "version-16")
     }
 
     if latest == current:
