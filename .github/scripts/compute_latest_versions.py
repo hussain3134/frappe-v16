fdd759863a71abeb6ca93f5419d780b1fd27f0eb
hussain3134 <95437954+hussain3134@users.noreply.github.com>
2026-02-19 12:30:40 +0530

    Add india_compliance version tracking to script

--- .github/scripts/compute_latest_versions.py
+++ .github/scripts/compute_latest_versions.py
@@ -68,7 +68,8 @@ def main():
     latest = {
         "erpnext": git_latest_tag("https://github.com/frappe/erpnext", "16"),
         "hrms": git_latest_tag("https://github.com/frappe/hrms", "16"),
-        "india_compliance": git_latest_tag("https://github.com/resilient-tech/india-compliance", "16"),
+        "india_compliance_ref": "refs/heads/version-16",
+        "india_compliance_sha": git_branch_sha("https://github.com/resilient-tech/india-compliance", "version-16")
         "crm": github_latest_release("frappe", "crm"),
         "ecommerce_integrations_ref": "refs/heads/version-16",
         "ecommerce_integrations_sha": git_branch_sha("https://github.com/frappe/ecommerce_integrations", "version-16")
